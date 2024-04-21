26283d1f1896542974a933ab6a74b5832932db87
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-04-21 16:05:43 -0500

    Se crea propiedad para acceder a la salud.
    Se crea la propiedad current_health usando métodos accesores.

--- blaster/scripts/base.gd
+++ blaster/scripts/base.gd
@@ -3,13 +3,20 @@ extends Node3D
 @export var max_health: int = 5
 @onready var label: Label3D = $label
 
-func _ready():
-	label.text = str(max_health)
+var current_health: int:
+	set(new_health):
+		current_health = new_health
+		label.text = str(current_health)
+
+		if current_health < 1:
+			get_tree().reload_current_scene()
+
+func _ready() -> void:
+	current_health = max_health
 
 func _process(delta: float) -> void:
 	pass
 
 func take_damage() -> void:
 	print('damage dealt to base!')
-	max_health -= 1
-	label.text = str(max_health)
+	current_health -= 1
