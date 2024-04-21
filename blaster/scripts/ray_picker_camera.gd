0f2e7fa308c8e8568b8907121fae872b27e51421
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-04-21 16:55:27 -0500

    Se cambia el color de la celda si apuntamos a ella con el mouse.

--- blaster/scripts/ray_picker_camera.gd
+++ blaster/scripts/ray_picker_camera.gd
@@ -1,5 +1,6 @@
 extends Camera3D
 
+@export var grid_map: GridMap
 @onready var ray_cast: RayCast3D = $ray_cast
 
 func _ready() -> void:
@@ -9,5 +10,13 @@ func _process(delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast.target_position = project_local_ray_normal(mouse_position) * 20.0
 	ray_cast.force_raycast_update()
-	
-	printt(ray_cast.get_collider(), ray_cast.get_collision_point())
+
+	if ray_cast.is_colliding():
+		var collider = ray_cast.get_collider()
+
+		if collider is GridMap:
+			var collision_point = ray_cast.get_collision_point()
+			var cell = grid_map.local_to_map(collision_point)
+			grid_map.set_cell_item(cell, 1)
+
+		printt(collider, ray_cast.get_collision_point())
