a294897a7447d7c23f3f8b6d6254cb59779ef2b7
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-04-21 19:53:08 -0500

    Si se cae de la plataforma, el escenario se reinicia.

--- robo_rampage/scripts/player.gd
+++ robo_rampage/scripts/player.gd
@@ -20,6 +20,9 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
+		if (position.y < -20.0):
+			get_tree().reload_current_scene()
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
--- robo_rampage/scripts/smooth_camera.gd
+++ robo_rampage/scripts/smooth_camera.gd
@@ -3,7 +3,7 @@ extends Camera3D
 @export var speed: float = 44.0
 
 func _physics_process(delta: float) -> void:
-	var weight = clamp(delta * speed, 0.1, 1.0)
+	var weight = clamp(delta * speed, 0.0, 1.0)
 
 	global_transform = global_transform.interpolate_with(get_parent().global_transform, weight)
 	global_position = get_parent().global_position
