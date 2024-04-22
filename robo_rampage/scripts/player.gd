18232986b8477fb92b1203ff2aeed4f1b2be6f75
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-04-21 19:38:17 -0500

    Se configura la cámara para que su movimiento sea más suave.

--- robo_rampage/scripts/player.gd
+++ robo_rampage/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
+@onready var camera_pivot: Node3D = $camera_pivot
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -39,11 +40,13 @@ func _physics_process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		mouse_motion = -event.relative * 0.001
+		mouse_motion = -event.relative * 0.004
 
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clamp(camera_pivot.rotation_degrees.x, -90.0, 90.0)
 	mouse_motion = Vector2.ZERO
